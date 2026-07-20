45e153cb421f18e8d1b2235be6ad23cf4bb099d9
Diyorbek Qodirboyev <dvrk.devx@gmail.com>
2026-07-20 13:42:53 +0500

    Implement user login functionality with form validation and feedback

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,8 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_user
+from werkzeug.security import check_password_hash
+
+from app.forms import LoginForm
+from app.models import User
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
 @bp.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("auth/login.html")
+    if current_user.is_authenticated:
+        return redirect(url_for("main.index"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password_hash, form.password.data):
+            login_user(user)
+            flash("Welcome back!", "info")
+            next_page = request.args.get("next", None)
+            return redirect(next_page or url_for("main.index"))
+        else:
+            flash("Email or password incorrect!", "danger")
+    return render_template("auth/login.html", form=form)
