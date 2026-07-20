43b5eb0596ff4850600e586b474ec381e5ad3ddc
Diyorbek Qodirboyev <dvrk.devx@gmail.com>
2026-07-20 12:22:18 +0500

    Add authentication routes and login template for user login functionality

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 import config
 from app import extensions as ext
+from app.models import User
+from app.routes import auth, main
 
 
 def create_app():
@@ -15,11 +17,21 @@ def create_app():
 
     # initialize extensions
     ext.db.init_app(app)
-    # ext.login_manager.init_app(app)
+    ext.login_manager.init_app(app)
     ext.csrf.init_app(app)
 
-    from app.routes import main
+    # login manager configurations
+    ext.login_manager.login_message = "Welcome back!"
+    ext.login_manager.login_message_category = "info"
+    # ext.login_manager.login_view = ""
 
+    # register blueprints
     app.register_blueprint(main.bp)
+    app.register_blueprint(auth.bp)
+
+    # set user loader
+    @ext.login_manager.user_loader
+    def load_user(user_id: int):
+        return User.query.get(user_id)
 
     return app
---
+++ app/routes/auth.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("auth/login.html")
