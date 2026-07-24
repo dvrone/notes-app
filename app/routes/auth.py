17c37e3c15020bd028bd3e45646f5d52da8ba340
Diyorbek Qodirboyev <dvrk.devx@gmail.com>
2026-07-24 23:52:17 +0500

    Registration route created

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_user
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from app.forms import LoginForm, RegisterForm
 from app.models import User
@@ -27,5 +27,17 @@ def login():
 
 @bp.route("/register", methods=["GET", "POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("main.index"))
     form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data)
+        user = User(
+            name=form.name.data, email=form.email.data, password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+        flash("Your account created successfully", "success")
+        return redirect(url_for("main.index"))
     return render_template("auth/register.html", form=form)
